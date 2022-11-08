68a51d44491214399d9ead742f6e56a3a276e090
Suraj Gopal <surajcancode@gmail.com>
2022-11-08 14:18:53 -0500

    display all, create new, find/display by id all functioning

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  # before_action :set_task, only: [:show, :new, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    # raise
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  # retrieve form for new
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # retrieve a single task, display
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+
 end
