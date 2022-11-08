d5910ba0a7bcc5cc993d27e64e42a29bda3e2561
Suraj Gopal <surajcancode@gmail.com>
2022-11-08 14:41:19 -0500

    CRUD implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,6 @@ class TasksController < ApplicationController
   end
 
   def new
-    # raise
     @task = Task.new
   end
 
@@ -20,12 +19,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: :tasks
   # retrieve form for new
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
 
   # retrieve a single task, display
   get '/tasks/:id', to: 'tasks#show', as: :task
+  # edit a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # send form with new task info
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # destroy a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 
 end
